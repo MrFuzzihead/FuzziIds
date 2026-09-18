5ecff73349e46250e43dbf9873388348e453163a
Duncan Campbell <campbellduncan12@gmail.com>
2026-09-18 09:21:04 -0500

    Initial commit

---
+++ src/main/java/com/mrfuzzihead/fuzziids/mixins/EarlyMixinsLoader.java
@@ -0,0 +1,51 @@
+package com.mrfuzzihead.fuzziids.mixins;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import net.minecraft.launchwrapper.Launch;
+
+import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+@IFMLLoadingPlugin.MCVersion("1.7.10")
+public class EarlyMixinsLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {}
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.fuzziids.early.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedCoreMods) {
+        // Config.synchronizeConfiguration(new File(Launch.minecraftHome, "config/fuzziids.cfg"));
+        return IMixins.getEarlyMixins(Mixins.class, loadedCoreMods);
+    }
+}
---
+++ src/main/java/com/mrfuzzihead/fuzziids/mixins/Mixins.java
@@ -0,0 +1,24 @@
+package com.mrfuzzihead.fuzziids.mixins;
+
+import javax.annotation.Nonnull;
+
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+import com.gtnewhorizon.gtnhmixins.builders.MixinBuilder;
+
+public enum Mixins implements IMixins {
+
+    MINECRAFT(new MixinBuilder().setPhase(Phase.EARLY)
+        .addCommonMixins("")),
+
+    private final MixinBuilder builder;
+
+    Mixins(MixinBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Nonnull
+    @Override
+    public MixinBuilder getBuilder() {
+        return builder;
+    }
+}
